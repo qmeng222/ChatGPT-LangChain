5cb934a1b69116f6ba466073cc07b3e7ba961106
Qingying Meng <qmeng222@gmail.com>
2023-12-22 17:32:49 -0800

    added functionality to receive and display user input

---
+++ tchat/main.py
@@ -0,0 +1,5 @@
+while True: # infinite loop
+  content = input(">> ") # input() function is used to read a line from the user
+  print(f"You entered: {content}")
+
+# press Ctrl+C to stop the infinite loop
