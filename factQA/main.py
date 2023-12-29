88f97f456d7a751d4104d4af0cbe6971df031ca8
Qingying Meng <qmeng222@gmail.com>
2023-12-28 22:27:19 -0800

    used the load_and_split method of the TextLoader instance to split the loaded text data

--- factQA/main.py
+++ factQA/main.py
@@ -1,14 +1,37 @@
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv # load variables from .env
 
 
-# ------ MODEL ------
 # load variables from a file named .env:
 load_dotenv()
 # load_dotenv(dotenv_path=".env2") # load variables from a file named .env2
 
 
+# ------ TEXT SPLITTER ------
+text_splitter = CharacterTextSplitter(
+  separator="\n",
+  chunk_size=200, # each chunk of text will contain at most 200 characters
+  chunk_overlap=0 # number of chars by which consecutive chunks can overlap (no overlap between chunks)
+  # chunk_overlap=100 # play with this parameter & see how it works
+)
+
+
 # ------ DOCUMENT LOADERS ------
-loader = TextLoader("facts.txt") # an object that can load text data from the specified file path
-docs = loader.load() # call the load method of the TextLoader instance ("loader")
-print(docs) # [ Document(page_content='...', metadata={'source': 'facts.txt'}) ]
+# load text data from the specified file path:
+loader = TextLoader("facts.txt")
+
+# call the load_and_split method of the TextLoader instance ("loader"):
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+print(docs)
+# # list of sub documents:
+# [ Document(page_content="1. ...\n2. ...\n3. ...", metadata={'source': 'facts.txt'}),
+#   Document(page_content="4. ...\n5. ...",         metadata={'source': 'facts.txt'}),
+#   ... ]
+
+print("---------")
+for doc in docs:
+  print(doc.page_content, "\n")
